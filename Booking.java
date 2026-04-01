75abcb577a9493c521a0d7e589af8754c163c5fd
christiandalesabang <christiandalesabang@gmail.com>
2026-04-01 18:05:31 +0800

    Create Booking.java

---
+++ Booking.java
@@ -0,0 +1,35 @@
+public class Booking {
+
+    private int bookingId;
+    private int userId;
+    private int roomId;
+    private String date;
+    private String time;
+    private String status; // add this
+
+    public Booking(int bookingId, int userId, int roomId, String date, String time, String status) {
+        this.bookingId = bookingId;
+        this.userId = userId;
+        this.roomId = roomId;
+        this.date = date;
+        this.time = time;
+        this.status = status; // add this
+    }
+
+    public int getBookingId() { return bookingId; }
+    public int getUserId() { return userId; }
+    public int getRoomId() { return roomId; }
+    public String getDate() { return date; }
+    public String getTime() { return time; }
+    public String getStatus() { return status; } // add this
+
+    public void display() {
+        System.out.println("Booking ID : " + bookingId);
+        System.out.println("User ID    : " + userId);
+        System.out.println("Room ID    : " + roomId);
+        System.out.println("Date       : " + date);
+        System.out.println("Time       : " + time);
+        System.out.println("Status     : " + status); // add this
+        System.out.println("---------------------------");
+    }
+}
